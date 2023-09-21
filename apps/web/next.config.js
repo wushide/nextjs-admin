1654d56045093e1fbe70cac281505339f767cb99
wushide <1747583024@qq.com>
2023-09-21 11:38:55 +0800

    Initial commit

---
+++ apps/web/next.config.js
@@ -0,0 +1,12 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: wushide
+ * @Date: 2023-09-21 08:28:10
+ * @LastEditors: wushide
+ * @LastEditTime: 2023-09-21 08:28:14
+ */
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
